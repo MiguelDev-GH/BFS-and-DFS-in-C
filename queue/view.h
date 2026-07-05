acd81d8228248d1f1d1ba9dccf24adb911757551
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-07-04 23:48:23 -0300

    fix(view): errors messages

--- queue/view.h
+++ queue/view.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define msg_queue_initializing_error printf("Error to acess the queue, maybe not initialized")
-#define msg_node_initializing_error printf("Error to create a new node")
+#define msg_queue_initializing_error printf("Error to acess the queue, maybe not initialized\n")
+#define msg_node_initializing_error printf("Error to create a new node\n")
 
-#define msg_pop_empty_queue_error printf("Error to pop a element, empty queue");
+#define msg_pop_empty_queue_error printf("Error to pop a element, empty queue\n");
 
 typedef struct node{
     int value;
